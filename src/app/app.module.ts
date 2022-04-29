d7ebb7d12665a8cfa8c2fa7279e9704393e0a510
BijanEisapour <bijanprogrammer@gmail.com>
2022-04-29 21:53:27 +0430

    [ADD] animation to requirements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import {ComponentsModule} from './components/components.module';
 import {PagesModule} from './pages/pages.module';
 import {PipesModule} from './pipes/pipes.module';
 
+import {FlipService} from './services/flip.service';
+
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule, ComponentsModule, PagesModule, PipesModule],
-    providers: [],
+    providers: [FlipService],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/requirements/requirements.component.ts
+++ src/app/pages/requirements/requirements.component.ts
@@ -1,8 +1,56 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-requirements',
     templateUrl: './requirements.component.html',
     styleUrls: ['./requirements.component.scss'],
 })
-export class RequirementsComponent {}
+export class RequirementsComponent implements AfterViewInit {
+    private readonly MINIMUM_DELAY: number = 0;
+    private readonly MAXIMUM_DELAY: number = 1000;
+
+    @ViewChild('frontendSection') private frontendSection!: ElementRef<HTMLElement>;
+    @ViewChild('softwareSection') private softwareSection!: ElementRef<HTMLElement>;
+
+    private observer!: IntersectionObserver;
+
+    public ngAfterViewInit(): void {
+        this.initializeIntersectionObserver();
+    }
+
+    private initializeIntersectionObserver(): void {
+        this.observer = new IntersectionObserver(
+            (entries: IntersectionObserverEntry[]) => {
+                entries.forEach((entry) => {
+                    const element: HTMLElement | null = entry.target as HTMLElement;
+                    if (!element || !entry.isIntersecting) return;
+
+                    const delay = element.tagName === 'LI' ? this.generateRandomDelay() : 0;
+                    setTimeout(() => element.classList.add('fade-in'), delay);
+
+                    this.observer.unobserve(element);
+                });
+            },
+            {
+                rootMargin: '-100px 0px',
+                threshold: 1,
+            }
+        );
+
+        this.observeElements(this.frontendSection.nativeElement);
+        this.observeElements(this.softwareSection.nativeElement);
+    }
+
+    private observeElements(section: HTMLElement): void {
+        const items: HTMLElement[] = Array.from(section.querySelectorAll('header, h3, li'));
+        console.log(items);
+
+        items.forEach((item) => {
+            this.observer.observe(item);
+        });
+    }
+
+    private generateRandomDelay(): number {
+        return Math.random() * (this.MAXIMUM_DELAY - this.MINIMUM_DELAY) + this.MINIMUM_DELAY;
+    }
+}
