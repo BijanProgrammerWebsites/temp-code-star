7480f3d9de8b0175fb6307e2e95b80d9d90c5a6a
BijanEisapour <bijanprogrammer@gmail.com>
2022-04-29 18:09:48 +0430

    [ADD] content

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,16 +4,33 @@ import {RouterModule} from '@angular/router';
 
 import {ComponentsModule} from '../components/components.module';
 import {PipesModule} from '../pipes/pipes.module';
+import {LogosModule} from '../logos/logos.module';
 
 import {HeroComponent} from './hero/hero.component';
 import {FaqComponent} from './faq/faq.component';
 import {NumbersComponent} from './numbers/numbers.component';
 import {TimelineComponent} from './timeline/timeline.component';
 import {RequirementsComponent} from './requirements/requirements.component';
+import {ContentComponent} from './content/content.component';
+import {TechnologiesModule} from '../technologies/technologies.module';
 
 @NgModule({
-    declarations: [HeroComponent, FaqComponent, NumbersComponent, TimelineComponent, RequirementsComponent],
-    imports: [CommonModule, RouterModule, ComponentsModule, PipesModule],
-    exports: [HeroComponent, FaqComponent, NumbersComponent, TimelineComponent, RequirementsComponent],
+    declarations: [
+        HeroComponent,
+        FaqComponent,
+        NumbersComponent,
+        TimelineComponent,
+        RequirementsComponent,
+        ContentComponent,
+    ],
+    imports: [CommonModule, RouterModule, ComponentsModule, PipesModule, LogosModule, TechnologiesModule],
+    exports: [
+        HeroComponent,
+        FaqComponent,
+        NumbersComponent,
+        TimelineComponent,
+        RequirementsComponent,
+        ContentComponent,
+    ],
 })
 export class PagesModule {}
