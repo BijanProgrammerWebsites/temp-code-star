65e480de2a31b94f8051b1b9b7fc0dee9c22de8a
BijanEisapour <bijanprogrammer@gmail.com>
2022-04-29 14:39:47 +0430

    [UPDATE] header styles

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, Renderer2} from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnDestroy {
+    public headerClass: string = '';
+
+    private destroyScrollListener!: () => void;
+    private scrollTop!: number;
+
+    public constructor(private renderer2: Renderer2) {
+        this.initializeScrollListener();
+    }
+
+    public ngOnDestroy(): void {
+        this.destroyScrollListener();
+    }
+
+    private initializeScrollListener(): void {
+        this.destroyScrollListener = this.renderer2.listen('window', 'scroll', (e) => {
+            const scrollingElement = (e.target as Document)?.scrollingElement;
+            if (!scrollingElement) return;
+
+            this.scrollHandler(scrollingElement.scrollTop);
+        });
+    }
+
+    private scrollHandler(scrollTop: number): void {
+        if (scrollTop > this.scrollTop) {
+            this.headerClass = 'fade-out';
+        } else if (scrollTop !== 0) {
+            this.headerClass = 'fade-in';
+        } else {
+            this.headerClass = '';
+        }
+
+        this.scrollTop = scrollTop;
+    }
+}
