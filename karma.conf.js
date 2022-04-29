e44ee30f5254c69ad523330f61b15bad0824e9cb
BijanEisapour <bijanprogrammer@gmail.com>
2022-04-29 08:46:08 +0430

    [INIT]

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
             suppressAll: true, // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/boilera'),
+            dir: require('path').join(__dirname, './coverage/code-star'),
             subdir: '.',
             reporters: [{type: 'html'}, {type: 'text-summary'}],
         },
