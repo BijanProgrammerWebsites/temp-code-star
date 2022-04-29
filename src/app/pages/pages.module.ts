8e805130383e562c15369113b40bc508f3ab6256
BijanEisapour <bijanprogrammer@gmail.com>
2022-04-29 11:43:23 +0430

    [ADD] numbers

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,10 +7,11 @@ import {PipesModule} from '../pipes/pipes.module';
 
 import {HeroComponent} from './hero/hero.component';
 import {FaqComponent} from './faq/faq.component';
+import {NumbersComponent} from './numbers/numbers.component';
 
 @NgModule({
-    declarations: [HeroComponent, FaqComponent],
+    declarations: [HeroComponent, FaqComponent, NumbersComponent],
     imports: [CommonModule, RouterModule, ComponentsModule, PipesModule],
-    exports: [HeroComponent, FaqComponent],
+    exports: [HeroComponent, FaqComponent, NumbersComponent],
 })
 export class PagesModule {}
