a8009841233cb735cef2b731242152bddf2d319e
BijanEisapour <bijanprogrammer@gmail.com>
2022-04-29 12:24:43 +0430

    [ADD] timeline

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,10 +8,11 @@ import {PipesModule} from '../pipes/pipes.module';
 import {HeroComponent} from './hero/hero.component';
 import {FaqComponent} from './faq/faq.component';
 import {NumbersComponent} from './numbers/numbers.component';
+import {TimelineComponent} from './timeline/timeline.component';
 
 @NgModule({
-    declarations: [HeroComponent, FaqComponent, NumbersComponent],
+    declarations: [HeroComponent, FaqComponent, NumbersComponent, TimelineComponent],
     imports: [CommonModule, RouterModule, ComponentsModule, PipesModule],
-    exports: [HeroComponent, FaqComponent, NumbersComponent],
+    exports: [HeroComponent, FaqComponent, NumbersComponent, TimelineComponent],
 })
 export class PagesModule {}
