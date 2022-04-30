bd09a9b52199720d298e13ba66cba2669fed6ed4
BijanEisapour <bijanprogrammer@gmail.com>
2022-04-30 11:27:18 +0430

    [ADD] animation to concepts

--- src/app/pages/concepts/concepts.component.ts
+++ src/app/pages/concepts/concepts.component.ts
@@ -1,8 +1,18 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {IntersectionObserverService} from '../../services/intersection-observer.service';
 
 @Component({
     selector: 'app-concepts',
     templateUrl: './concepts.component.html',
     styleUrls: ['./concepts.component.scss'],
 })
-export class ConceptsComponent {}
+export class ConceptsComponent implements AfterViewInit {
+    @ViewChild('section') private section!: ElementRef<HTMLElement>;
+
+    public constructor(private service: IntersectionObserverService) {}
+
+    public ngAfterViewInit(): void {
+        const options: IntersectionObserverInit = {rootMargin: '-120px 0px'};
+        this.service.initObserver(this.section.nativeElement, 'h3, p', options);
+    }
+}
